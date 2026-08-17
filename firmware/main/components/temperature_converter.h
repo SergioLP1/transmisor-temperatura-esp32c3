e02b6b90afeab37a06b8367e772273437440c053
Sergio <199535664+SergioLP1@users.noreply.github.com>
2026-08-17 16:25:41 -0400

    Firmware

--- firmware/main/components/temperature_converter.h
+++ firmware/main/components/temperature_converter.h
@@ -4,9 +4,9 @@
 #include "esp_log.h"
 #include <math.h>
 
-#define MAX6675_MISO_GPIO 37
-#define MAX6675_SCK_GPIO 36
-#define MAX6675_CS_GPIO 35
+#define MAX6675_MISO_GPIO 5
+#define MAX6675_SCK_GPIO 4
+#define MAX6675_CS_GPIO 7
 
 void max6675_init(void);
 
